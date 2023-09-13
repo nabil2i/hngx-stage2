723107e0be1603a16a84e385479de774c8c0f3ab
Nabil Affo <nabil.noble01@gmail.com>
2023-09-13 17:41:48 +0000

    setup for vercel

--- hngxtwo/settings/dev.py
+++ hngxtwo/settings/dev.py
@@ -10,11 +10,22 @@ SECRET_KEY = 'django-insecure-1n8!k4@&m$5bhzj*(9fu-#p(e!2ewzu^zfqnn+%hmen4m_n6kw
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'railway',
+    'USER': 'postgres',
+    'PASSWORD': '3wZtIfS92YtCfVFstC54',
+    'HOST': 'containers-us-west-185.railway.app',
+    'PORT': '7064',
+  }
 }
 
 # DATABASES = {
--- hngxtwo/settings/prod.py
+++ hngxtwo/settings/prod.py
@@ -10,11 +10,22 @@ ALLOWED_HOSTS = ['*']
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'railway',
+    'USER': 'postgres',
+    'PASSWORD': '3wZtIfS92YtCfVFstC54',
+    'HOST': 'containers-us-west-185.railway.app',
+    'PORT': '7064',
+  }
 }
 
 # DATABASES = {
